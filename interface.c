bbb8ef97c5460d39efe7383aee93771a4e0b83ea
Edisson López <ediloaz@gmail.com>
2021-05-02 11:31:06 -0600

    Interfaz con funciones para definir Productores y Consumidores activos

--- interface.c
+++ interface.c
@@ -1,11 +1,43 @@
 #include <gtk/gtk.h>
+#include<stdbool.h>  
+
+
+/* COMANDO PARA CORRER:
+gcc -o gladewin interface.c -lm -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic ; ./gladewin
+*/
+
+
+//Inicialización de los componentes en Interfaz
+
+GtkBuilder *builder;
+GtkWidget *window;
+GtkWidget *g_lbl_nombreBuffer;
+GtkWidget *g_lbl_cantidadProductoresActivos;
+GtkWidget *g_lbl_cantidadConsumidoresActivos;
+GtkWidget *g_txt_bitacora;
+
+// Función auxiliar
+void _RenderizarCantidadProductoresOConsumidoresActivos(int cantidad, bool esProductor){
+    char stringValue[10];   // stringValue Debe ser lo bastante grande
+    sprintf(stringValue, "%i", cantidad);
+    gtk_label_set_text(GTK_LABEL(esProductor ? g_lbl_cantidadProductoresActivos : g_lbl_cantidadConsumidoresActivos ), stringValue);
+}
+
+// Llamar esta función para RENDERIZAR/PINTAR el número de PRODUCTORES en la pantalla
+void RenderizarCantidadProductoresActivos(int cantidad)
+{
+    _RenderizarCantidadProductoresOConsumidoresActivos(cantidad, true);
+}
+
+// Llamar esta función para RENDERIZAR/PINTAR el número de CONSUMIDORES en la pantalla
+void RenderizarCantidadConsumidoresActivos(int cantidad)
+{
+    _RenderizarCantidadProductoresOConsumidoresActivos(cantidad, false);
+}
 
 // Creación de la interfaz
 void IniciarInterfaz(int argc, char *argv[])
 {
-    GtkBuilder *builder;
-    GtkWidget *window;
-
     gtk_init(&argc, &argv);
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "interface.glade", NULL);
@@ -19,9 +51,29 @@ void IniciarInterfaz(int argc, char *argv[])
         GTK_STYLE_PROVIDER(cssProvider),
         GTK_STYLE_PROVIDER_PRIORITY_USER);
 
+    
+    // Referencia de los componentes en interfaz que se ocupan manejar con código
+    g_lbl_nombreBuffer = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_nombreBuffer"));
+    g_lbl_cantidadProductoresActivos = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_cantidadProductoresActivos"));
+    g_lbl_cantidadConsumidoresActivos = GTK_WIDGET(gtk_builder_get_object(builder, "lbl_cantidadConsumidoresActivos"));
+    g_txt_bitacora = GTK_WIDGET(gtk_builder_get_object(builder, "txt_bitacora"));
+    
+    
+    // Ejemplo de Renderizar/Pintar en interfaz
+    RenderizarCantidadProductoresActivos(9);
+    RenderizarCantidadConsumidoresActivos(180);
+
     g_object_unref(builder);
     gtk_widget_show(window);
     gtk_main();
+}
+
+
+// Se llama cuando la interfaz es cerrada
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+    exit(0);
 }
 
 int main(int argc, char **argv)
